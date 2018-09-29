9b2961d8c993365acf531708625ab1e5fe2c5293
unknown <s14113101@stu.edu.tw>
2018-09-29 21:32:20 +0800

    use wifi

--- bpi_uno32/example/leap year.py
+++
@@ -1,5 +0,0 @@
-def is_leap_year(year):
-	if (year % 4) == 0 and (year % 100) != 0 or (year % 400) == 0:
-		print("{0} is leap year".format(year))
-	else:
-		print("{0} not is leap year".format(year))
