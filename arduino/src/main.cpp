9e69edd201e3d89ecf78f666cd250038e104f1a3
ayu0108 <s17113201@stu.edu.tw>
2018-10-11 08:53:42 +0800

    Led Flash & ADC Control

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SPIFlash.h>
+#include <STM32ADC.h>
 
 // ~/.platformio/packages/framework-arduinoststm32/STM32F1/variants/generic_stm32f103r8/board.cpp
 
@@ -21,7 +22,14 @@ SPIFlash flash(FLASH_SS, FLASH_ID);
 
 uint8_t LEDS[] = {BC95_LED1,BC95_LED2,BC95_LED3,BC95_LED4};
 unsigned long counter = 0;
+unsigned long LedCounter = 0;                                                                            
 unsigned long lastTime;
+
+uint8 pin = PB0;
+
+STM32ADC myADC(ADC1);
+
+uint16_t dataPoint;
 
 bool Flash_Ready = false;
 
@@ -31,8 +39,8 @@ void setup() {
 
    afio_remap(AFIO_REMAP_USART3_PARTIAL);
    gpio_set_mode(GPIOC, 10, GPIO_AF_OUTPUT_PP);
-    gpio_set_mode(GPIOC, 11, GPIO_INPUT_FLOATING);
-    gpio_set_mode(GPIOC, 12, GPIO_AF_OUTPUT_PP);
+   gpio_set_mode(GPIOC, 11, GPIO_INPUT_FLOATING);
+   gpio_set_mode(GPIOC, 12, GPIO_AF_OUTPUT_PP);
 
    Serial3.begin(9600);
    Serial3.println("hello im Serial-3");
@@ -40,17 +48,28 @@ void setup() {
    if (flash.initialize())
    {
        Flash_Ready = true;
-       Serial1.println("Init OK!");
+       Serial1.println("Flash Init OK!");
    } else {
        Flash_Ready = false;
-       Serial1.print("Init FAIL!");
+       Serial1.print("Flash Init FAIL!");
        Serial1.println(flash.readDeviceId(),HEX);
    }
 
+   pinMode(pin,INPUT_ANALOG);
+ 
+   myADC.setSampleRate(ADC_SMPR_1_5);//set the Sample Rate
+   myADC.setScanMode();              //set the ADC in Scan mode. 
+   myADC.setPins(&pin, 1);           //set how many and which pins to convert.
+   myADC.setContinuous();            //set the ADC in continuous mode.
+   
+   myADC.setDMA(&dataPoint, 1, (DMA_MINC_MODE | DMA_CIRC_MODE), NULL);
+
+   myADC.startConversion();
+
    for(int i=0;i<sizeof(LEDS);i++){
-		pinMode(LEDS[i], OUTPUT);
-		digitalWrite(LEDS[i], HIGH);
-	}
+       pinMode(LEDS[i], OUTPUT);
+       digitalWrite(LEDS[i], HIGH);
+   }
 }
 
 String BC95_Buffer = "";
@@ -121,20 +140,69 @@ void loop() {
        }
 
     //UART1
-   if (Serial1.available() > 0) {
-       int incomingByte = Serial1.read();
-        BC95_Buffer.concat((char)incomingByte);
-        Serial2.print(BC95_Buffer);
-        BC95_Buffer = "";
-       //Serial1.print("Serial1 Recevied : ");
-       //Serial1.println(incomingByte, HEX);               
-        
-   }
+	if (Serial1.available() > 0) {
+		int incomingByte = Serial1.read();
+		Serial1.print("Serial1 Recevied : ");
+		Serial1.println(incomingByte, HEX);
+
+		if(Flash_Ready) {
+			if(incomingByte == 'a') {
+				Serial1.print("Erasing Flash chip ");
+				flash.chipErase();
+				while(flash.busy()) {
+					Serial1.print(".");
+					delay(100);
+				}
+				Serial1.println("DONE");
+			}
+			if(incomingByte == 0x31) {
+				char test[] = "this is a test\0";
+				for(int i = 0;i<=sizeof(test);i++) {
+					flash.writeByte(i,test[i]);
+				}
+				Serial1.println("Write Flash Data : \"" + String(test) + "\" Done");
+			}
+			if(incomingByte == 0x32) {
+				String strFlash = "";
+				uint8_t chr = 0;
+				counter = 0;
+				while(true) {
+					chr = flash.readByte(counter);
+					if(chr == 0xff || chr == 0x0)
+						break;
+					strFlash.concat((char)chr);
+					counter++;
+				}
+				Serial1.print("Flash Data : ");
+				if(strFlash.length() == 0) {
+					Serial1.println("(null)");
+				} else {
+					Serial1.println(strFlash);
+				}
+			}
+			if(incomingByte == 0x33) {
+				Serial1.println("ADC Value : " + String(dataPoint));
+			}
+		}
+	}
 
    //RS485
    if (Serial3.available() > 0) {
         int incomingByte = Serial3.read();
-        Serial3.print("Serial 3 Recevied : ");
-        Serial3.println(incomingByte, HEX);
+        Serial1.print("Serial 3 Recevied : ");
+        Serial1.println(incomingByte, HEX);
     }
+
+   if(millis() - lastTime >= 1000) {
+
+        for(int i=0;i<sizeof(LEDS);i++){
+             if(i == LedCounter % sizeof(LEDS)) {
+		digitalWrite(LEDS[i], LOW);
+	     } else {
+	        digitalWrite(LEDS[i], HIGH);
+             }
+        }
+	lastTime = millis();
+	LedCounter++;
+   }
 }
